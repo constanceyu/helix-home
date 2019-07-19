69173c97858a564d1f0e2c95c02006b542c8a6b1
David Nuescheler <david@nuescheler.ch>
2019-07-19 14:22:08 -0600

    setup initial helix pages rendering and idx hack

---
+++ src/idx_html.pre.js
@@ -0,0 +1,64 @@
+const jquery = require('jquery');
+const { preFetch } = require('./utils.js');
+
+/**
+ * The 'pre' function that is executed before the HTML is rendered
+ * @param context The current context of processing pipeline
+ * @param context.content The content
+ */
+function pre(context) {
+  const { document } = context.content;
+  const $ = jquery(document.defaultView);
+
+  const $sections = $(document.body).children('div');
+
+  // first section has a starting image: add title class and wrap all subsequent items inside a div
+  $sections
+    .first()
+    .has('p:first-child>img')
+    .addClass('title')
+    .find(':nth-child(1n+2)')
+    .wrapAll('<div class="header"></div>');
+
+  // sections consisting of only one image
+  $sections
+    .filter('[data-hlx-types~="has-only-image"]')
+    .not('.title')
+    .addClass('image');
+
+  // sections without image and title class gets a default class
+  $sections
+    .not('.image')
+    .not('.title')
+    .addClass('default');
+
+  // if there are no sections wrap everything in a default div
+  if ($sections.length === 0) {
+    $(document.body).children().wrapAll('<div class="default"></div>');
+  }
+
+  // construct the tables
+  var tables=[];
+  var simple={name:'simple', entries: {}};
+
+  var titleEl=document.querySelector('h1');
+  if (titleEl) {
+    simple.entries['title']=titleEl.textContent;
+  }
+
+  var descEl=document.querySelector('.title .header p');
+  if (descEl) {
+    simple.entries['description']=descEl.textContent;
+  }
+
+
+  tables.push(simple);
+  context.content.json={tables: tables};
+
+  context.content.json.string=JSON.stringify(context.content.json);
+}
+
+module.exports.pre = pre;
+module.exports.before = {
+  fetch: preFetch,
+};
