c30afb6dc53bb0b3a07ccc75a40f742abbdf4c73
constance yu <faithwyu@gmail.com>
2019-07-31 10:12:39 -0700

    added bot

---
+++ helix-scanner-bot/index.js
@@ -0,0 +1,18 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('push', async context => {
+    console.log(context)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
